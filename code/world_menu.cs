f6cafb959bc692d567a24a1121c89059377195e8
miicck <michael.hutcheon@hotmail.co.uk>
2020-03-31 17:46:01 +0100

    Updates to world creation menu

--- code/world_menu.cs
+++ code/world_menu.cs
@@ -2,13 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Security.Cryptography;
+
+// Used to highlight an incorrect input
+public class error_highlighter : MonoBehaviour
+{
+    public static void highlight(Image image, float fade_duration=1.0f)
+    {
+        var existing = image.gameObject.GetComponent<error_highlighter>();
+        if (existing != null)
+        {
+            image.color = existing.reset_color;
+            Destroy(existing);
+        }
+
+        var eh = image.gameObject.AddComponent<error_highlighter>();
+        eh.initial_color = Color.red;
+        eh.reset_color = image.color;
+        eh.start_time = Time.realtimeSinceStartup;
+        eh.image = image;
+        eh.duration = fade_duration;
+    }
+
+    Color initial_color;
+    Color reset_color;
+    float start_time;
+    float duration;
+    Image image;
+
+    private void Update()
+    {
+        float t = Time.realtimeSinceStartup - start_time;
+        t /= duration;
+
+        image.color = Color.Lerp(initial_color, reset_color, t);
+        if (t >= 1.0f)
+        {
+            image.color = reset_color;
+            Destroy(this);
+        }
+    }
+}
 
 public class world_menu : MonoBehaviour
 {
     public Button template_button;
+    public Transform template_world_button;
     public Text template_header;
     public Transform button_container;
     public Camera menu_camera;
+    public InputField world_name_input;
+    public InputField world_seed_input;
 
     private void Start()
     {
@@ -20,31 +64,78 @@ public class world_menu : MonoBehaviour
         if (world_folders.Length == 0) load_header.text = "No existing worlds";
         load_header.transform.SetParent(button_container);
 
+        int world_count = world_folders.Length;
+
         foreach (var wf in world_folders)
         {
-            var button = template_button.inst();
+            var button = template_world_button.inst();
             button.transform.SetParent(button_container);
 
             string[] splt = wf.Split('/');
-            button.GetComponentInChildren<Text>().text = splt[splt.Length - 1];
-            button.onClick.AddListener(() => game.load_world(wf));
+
+            var load_button = button.Find("load").GetComponent<Button>();
+            load_button.GetComponentInChildren<Text>().text = splt[splt.Length - 1];
+            load_button.onClick.AddListener(() => game.load_world(wf));
+
+            var delete_button = button.Find("delete").GetComponent<Button>();
+            delete_button.onClick.AddListener(() =>
+            {
+                Destroy(button.gameObject);
+                System.IO.Directory.Delete(wf, true);
+                world_count -= 1;
+                if (world_count == 0)
+                    load_header.text = "No existing worlds";
+            });
         }
 
-        if (world_folders.Length > 0)
-        {
-            var create_header = template_header.inst();
-            create_header.text = "Create new world...";
-            create_header.transform.SetParent(button_container);
-        }
+        var create_header = template_header.inst();
+        create_header.text = "Create new world...";
+        create_header.transform.SetParent(button_container);
 
+        // Move world name/seed inputs to bottom
+        world_name_input.transform.SetAsLastSibling();
+        world_seed_input.transform.SetAsLastSibling();
+
+        // Create new world button
         var new_world = template_button.inst();
         new_world.transform.SetParent(button_container);
         new_world.GetComponentInChildren<Text>().text = "New world";
-        new_world.onClick.AddListener(() => game.create_world("world", 6969));
+        new_world.onClick.AddListener(() =>
+        {
+            string name = world_name_input.text.Trim();
+            string folder = world.worlds_folder() + "/" + name;
+            if (System.IO.Directory.Exists(folder))
+            {
+                error_highlighter.highlight(world_name_input.GetComponent<Image>());
+                return;
+            }
+
+            // Random seed if no seed specified
+            // or directly parse seed from input
+            // or hash seed from input
+            int seed = Random.Range(0, int.MaxValue);
+            string ws = world_seed_input.text.Trim();
+            if (ws.Length > 0)
+            {
+                if (!int.TryParse(ws, out seed))
+                    seed = get_hash(ws);
+            }
+
+            game.create_world(name, seed);
+        });
 
         // Remove the templates
         Destroy(template_button.gameObject);
         Destroy(template_header.gameObject);
+        Destroy(template_world_button.gameObject);
+    }
+
+    public static int get_hash(string s)
+    {
+        byte[] hashed;
+        using (HashAlgorithm algorithm = SHA256.Create())
+            hashed = algorithm.ComputeHash(System.Text.Encoding.UTF8.GetBytes(s));
+        return System.BitConverter.ToInt32(hashed, 0);
     }
 
     private void Update()
