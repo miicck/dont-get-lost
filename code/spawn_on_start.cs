409e315c058b9abae1d702fe53d3f12a42ee0bcf
miicck <michael.hutcheon@hotmail.co.uk>
2020-04-09 17:46:18 +0100

    Added test scene for walkers.

---
+++ code/spawn_on_start.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_on_start : MonoBehaviour
+{
+    public GameObject to_spawn;
+    public int count;
+    public float time_betwen_spawns = 0;
+
+    int count_spawned = 0;
+    void spawn()
+    {
+        ++count_spawned;
+        to_spawn.inst().transform.position = transform.position;
+
+        if (count_spawned > count)
+            CancelInvoke();
+    }
+
+    void Start()
+    {
+        InvokeRepeating("spawn", 0, time_betwen_spawns);
+    }
+}
