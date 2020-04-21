22303c240f119586e480acd384ff378fe007a9d3
miicck <michael.hutcheon@hotmail.co.uk>
2020-04-21 20:20:30 +0100

    Generalized crafting system

--- code/crafting_input.cs
+++ code/crafting_input.cs
@@ -7,6 +7,7 @@ public class crafting_input : MonoBehaviour
     public Transform options_go_here;
     public inventory craft_from;
     public inventory craft_to;
+    public recipes.RECIPE_GROUP recipe_group;
 
     private void Start()
     {
@@ -18,64 +19,61 @@ public class crafting_input : MonoBehaviour
         foreach (Transform t in options_go_here)
             Destroy(t.gameObject);
 
-        foreach (var rec in recipes())
+        foreach (var rec in recipes.recipies(recipe_group))
             if (rec.can_craft(craft_from))
             {
                 var entry = rec.get_entry();
                 entry.transform.SetParent(options_go_here);
                 entry.button.onClick.AddListener(() =>
                 {
-                    int to_craft = 1;
-                    if (Input.GetKey(KeyCode.LeftShift))
-                        to_craft = 5;
+                    int to_craft = Input.GetKey(KeyCode.LeftShift) ? 5 : 1;
                     for (int n = 0; n < to_craft; ++n)
                         rec.craft(craft_from, craft_to);
                 });
             }
     }
-
-    recipe[] recipes()
-    {
-        return new recipe[]
-        {
-            new recipe("planks", 1, new ingredient("log",1)),
-            new recipe("stick",5, new ingredient("log",1))
-        };
-    }
 }
 
-class ingredient
+public abstract class ingredient
 {
-    string item_name;
-    int count;
+    public abstract string str();
+    public abstract bool in_inventory(inventory i);
+    public abstract void on_craft(inventory i);
 
-    public ingredient(string item_name, int count)
+    public class item : ingredient
     {
-        this.item_name = item_name;
-        this.count = count;
+        string item_name;
+        int count;
+
+        public item(string item_name, int count)
+        {
+            this.item_name = item_name;
+            this.count = count;
+        }
+
+        public override string str()
+        {
+            if (count > 1) return count + " " + item_name;
+            return item_name;
+        }
+
+        public override bool in_inventory(inventory i)
+        {
+            foreach (var s in i.slots)
+                if (s.item == item_name && s.count >= count)
+                    return true;
+            return false;
+        }
+
+        public override void on_craft(inventory i)
+        {
+            i.remove(item_name, count);
+        }
     }
 
-    public bool in_inventory(inventory i)
-    {
-        foreach (var s in i.slots)
-            if (s.item == item_name && s.count >= count)
-                return true;
-        return false;
-    }
-
-    public void on_craft(inventory i)
-    {
-        i.remove(item_name, count);
-    }
-
-    public string str()
-    {
-        if (count > 1) return count + " " + item_name;
-        return item_name;
-    }
 }
 
-class recipe
+public class recipe
 {
     public string product;
     public int product_count;
--- code/workbench.cs
+++ code/workbench.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class workbench : MonoBehaviour
 {
+    public recipes.RECIPE_GROUP recipe_group;
+
     inventory _inventory;
     public inventory inventory
     {
@@ -11,10 +13,12 @@ public class workbench : MonoBehaviour
         {
             if (_inventory == null)
             {
+                // Create the workbench inventory
                 _inventory = Resources.Load<inventory>("ui/workbench").inst();
                 _inventory.transform.SetParent(FindObjectOfType<Canvas>().transform);
                 _inventory.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
                 _inventory.GetComponentInChildren<UnityEngine.UI.Text>().text = GetComponent<item>().name.capitalize();
+                _inventory.GetComponent<crafting_input>().recipe_group = recipe_group;
             }
             return _inventory;
         }
