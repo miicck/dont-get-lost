fe6c8887c8218b823f0fa472d5c4f5db7066e4db
miicck <michael.hutcheon@hotmail.co.uk>
2020-06-03 20:51:47 +0100

    Fullscreen toggle is now a global control.

---
+++ code/global_controls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class global_controls : MonoBehaviour
+{
+    void Start()
+    {
+        // Persistant through scene loads
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        // Toggle fullscreen modes on F11
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            switch (Screen.fullScreenMode)
+            {
+                case FullScreenMode.Windowed:
+                    Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                    break;
+                case FullScreenMode.FullScreenWindow:
+                    Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                    break;
+                case FullScreenMode.ExclusiveFullScreen:
+                    Screen.fullScreenMode = FullScreenMode.Windowed;
+                    break;
+                default:
+                    throw new System.Exception("Unkown fullscreen mode!");
+            }
+        }
+    }
+}
