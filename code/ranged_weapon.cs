27d0845ffe49cc8b609e8e8eab06f4f45cd8d379
miicck <michael.hutcheon@hotmail.co.uk>
2020-06-15 14:38:38 +0100

    Right click with ranged weapon now picks up projectiles.

--- code/ranged_weapon.cs
+++ code/ranged_weapon.cs
@@ -8,7 +8,7 @@ public class ranged_weapon : equip_in_hand
     item_requirement ammunition { get => GetComponent<item_requirement>(); }
     public float projectile_velocity = 10f;
 
-    public override use_result on_use_start(player.USE_TYPE use_type)
+    void fire()
     {
         item ammo_found = null;
 
@@ -26,7 +26,7 @@ public class ranged_weapon : equip_in_hand
         if (ammo_found == null)
         {
             popup_message.create("No ammunition!");
-            return use_result.complete;
+            return;
         }
 
         player.current.inventory.contents.remove(ammo_found.name, 1);
@@ -42,6 +42,35 @@ public class ranged_weapon : equip_in_hand
         var rb = fired.gameObject.AddComponent<Rigidbody>();
         rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
         rb.velocity = player.current.camera.transform.forward * projectile_velocity;
+    }
+
+    void pickup_ammo()
+    {
+        var ray = player.current.camera_ray(player.INTERACTION_RANGE, out float distance);
+        foreach (var hit in Physics.RaycastAll(ray, distance))
+        {
+            var proj = hit.transform.GetComponent<projectile>();
+            if (proj != null) proj.pick_up();
+        }
+    }
+
+    public override bool allow_right_click_held_down()
+    {
+        return true;
+    }
+
+    public override use_result on_use_start(player.USE_TYPE use_type)
+    {
+        switch(use_type)
+        {
+            case player.USE_TYPE.USING_LEFT_CLICK:
+                fire();
+                break;
+
+            case player.USE_TYPE.USING_RIGHT_CLICK:
+                pickup_ammo();
+                break;
+        }
 
         return use_result.complete;
     }
