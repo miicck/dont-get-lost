d02889598953e294d2f4a02be306be1bb3fdf040
miicck <michael.hutcheon@hotmail.co.uk>
2020-06-16 08:17:31 +0100

    Improved comments.

--- code/console.cs
+++ code/console.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class console : MonoBehaviour
 {
+    /// <summary> The console in this game session. </summary>
     static console current;
 
+    /// <summary> True if the console window is open/selected. </summary>
     public static bool open
     {
         get => current.gameObject.activeInHierarchy;
@@ -17,14 +19,17 @@ public class console : MonoBehaviour
         }
     }
 
+    /// <summary> The input field where console commands are typed. </summary>
     UnityEngine.UI.InputField input;
 
     void Start()
     {
+        // Starts closed
         input = GetComponent<UnityEngine.UI.InputField>();
         current = this;
         open = false;
 
+        // Called when the player hits enter after typing a command
         input.onEndEdit.AddListener((string command) =>
         {
             process_command(command);
@@ -33,18 +38,22 @@ public class console : MonoBehaviour
         });
     }
 
+    /// <summary> Throw the given console error message 
+    /// to the player. Returns false. </summary>
     bool console_error(string msg)
     {
         popup_message.create(msg);
         return false;
     }
 
+    /// <summary> Process the given console command. </summary>
     bool process_command(string command)
     {
         var args = command.Split(null);
         
         switch(args[0])
         {
+            // Give the local player some items e.g [give 100 coin]
             case "give":
 
                 if (args.Length < 3) return console_error("Not enough arguments!");
