c8a8d8081ccd76ecb8105271db13bfda77d1c14c
miicck <michael.hutcheon@hotmail.co.uk>
2020-09-11 14:49:29 +0100

    Players now take damage from other players.

---
+++ code/player_hitbox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_hitbox : accepts_item_impact
+{
+    player player;
+    void Start()
+    {
+        player = GetComponent<player>();
+    }
+
+    public override bool on_impact(item i)
+    {
+        if (i is melee_weapon)
+        {
+            var mw = (melee_weapon)i;
+            player.take_damage(mw.damage);
+        }
+        return true;
+    }
+}
