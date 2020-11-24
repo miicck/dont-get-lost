a4f6ae806762eff06906955a8fc2d6a78142d394
miicck <michael.hutcheon@hotmail.co.uk>
2020-11-24 13:08:33 +0000

    Added additional inspect info to town gate.

--- Assets/code/town_gate.cs
+++ Assets/code/town_gate.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class town_gate : building_material
+public class town_gate : building_material, IAddsToInspectionText
 {
     public List<gate> gates = new List<gate>();
     public settler_path_element path_element;
+
+    int bed_count;
 
     private void Start()
     {
@@ -19,12 +21,18 @@ public class town_gate : building_material
             return;
 
         var elements = settler_path_element.element_group(path_element.group);
-        int bed_count = 0;
+        bed_count = 0;
         foreach (var e in elements)
             if (e.interactable is bed)
                 bed_count += 1;
 
         if (settler.settler_count < bed_count)
             client.create(transform.position, "characters/settler");
+    }
+
+    public string added_inspection_text()
+    {
+        return "Beds     : " + bed_count + "\n" +
+               "Settlers : " + settler.settler_count;
     }
 }
