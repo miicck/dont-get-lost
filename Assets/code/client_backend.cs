bc69276d495493550201894b13cd90ef817c293d
miicck <michael.hutcheon@hotmail.co.uk>
2021-11-28 18:34:14 +0000

    Comments for client/server backend

--- Assets/code/client_backend.cs
+++ Assets/code/client_backend.cs
@@ -4,24 +4,41 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
+/// <summary> The byte stream interface needed for 
+/// communication between the sever and clients. </summary>
 public abstract class backend_stream
 {
+    /// <summary> True if this stream supports reading 
+    /// (should basically always be the case, mainly included 
+    /// to accomodate the NetworkStream equivalent). </summary>
     public abstract bool CanRead { get; }
+
+    /// <summary> True if there is data waiting to be read from the stream. </summary>
     public abstract bool DataAvailable { get; }
+
+    /// <summary> Read data from the stream. </summary>
+    /// <param name="buffer">The buffer to read data into.</param>
+    /// <param name="start">The start location in the buffer to start writing to.</param>
+    /// <param name="count">The maximum number of bytes that can be read into the buffer.</param>
+    /// <returns>The number of bytes read from the stream.</returns>
     public abstract int Read(byte[] buffer, int start, int count);
+
+    /// <summary> Write data to the stream. </summary>
+    /// <param name="buffer">The buffer to write from.</param>
+    /// <param name="start">The start location in the buffer to start from.</param>
+    /// <param name="count">The number of bytes to write.</param>
     public abstract void Write(byte[] buffer, int start, int count);
+
+    /// <summary> Close the stream. </summary>
+    /// <param name="timeout_ms">The number of milliseconds to linger for before actually closing.</param>
     public abstract void Close(int timeout_ms);
 }
 
+/// <summary> TCP implementation of a <see cref="backend_stream"/>. </summary>
 public class tcp_stream : backend_stream
 {
     NetworkStream net_stream;
-
-    public tcp_stream(NetworkStream tcp_net_stream)
-    {
-        net_stream = tcp_net_stream;
-    }
-
+    public tcp_stream(NetworkStream tcp_net_stream) { net_stream = tcp_net_stream; }
     public override bool CanRead => net_stream.CanRead;
     public override bool DataAvailable => net_stream.DataAvailable;
     public override int Read(byte[] buffer, int start, int count) => net_stream.Read(buffer, start, count);
@@ -29,15 +46,29 @@ public class tcp_stream : backend_stream
     public override void Close(int timeout_ms) => net_stream.Close(timeout_ms);
 }
 
+/// <summary> The interface needed for the client to communicate with the server. </summary>
 public abstract class client_backend
 {
+    /// <summary> Close the connection to the server. </summary>
+    /// <param name="timeout_ms">The number of milliseconds to linger for before actually closing.</param>
     public abstract void Close(int timeout_ms = 0);
+
+    /// <summary> The byte stream used to communicate with the server. </summary>
     public abstract backend_stream stream { get; }
+
+    /// <summary> How many bytes should be allocated to store incoming messages. </summary>
     public abstract int ReceiveBufferSize { get; }
+
+    /// <summary> How many bytes should be allocated to store outgoing messages. </summary>
     public abstract int SendBufferSize { get; }
+
+    /// <summary> Called to start connecting to a server at the given address/port combo. </summary>
     public abstract IAsyncResult BeginConnect(string address, int port);
+
+    /// <summary> A string representing the address of this client (as seen by the server). </summary>
     public abstract string remote_address { get; }
 
+    /// <summary> The default backend used. </summary>
     public static client_backend default_backend()
     {
         return new tcp_client_backend();
@@ -45,6 +76,7 @@ public abstract class client_backend
     }
 }
 
+/// <summary> A TCP implementation of a <see cref="client_backend"/> </summary>
 public class tcp_client_backend : client_backend
 {
     TcpClient client;
@@ -85,6 +117,7 @@ public class tcp_client_backend : client_backend
     }
 }
 
+/// <summary> A Steamworks P2P-networking implemntation of a <see cref="backend_stream"/>. </summary>
 class steamworks_stream : backend_stream
 {
     Steamworks.SteamId id;
@@ -107,10 +140,7 @@ class steamworks_stream : backend_stream
     public override bool CanRead => true;
     public override bool DataAvailable => Steamworks.SteamNetworking.IsP2PPacketAvailable(channel: receive_channel);
 
-    public override void Close(int timeout_ms)
-    {
-
-    }
+    public override void Close(int timeout_ms) { } // Nothing to do
 
     public override int Read(byte[] buffer, int start, int count)
     {
@@ -132,6 +162,7 @@ class steamworks_stream : backend_stream
     }
 }
 
+/// <summary> A steamworks P2P-networking implementation of a <see cref="client_backend"/>. </summary>
 public class steamworks_client_backend : client_backend
 {
     public Steamworks.SteamId id { get; private set; }
@@ -142,22 +173,18 @@ public class steamworks_client_backend : client_backend
         id = steamId;
         this.server_side = server_side;
 
+        // This is a client-side client (i.e not the client that the server sees)
+        // we need to manually register it on the local server. In order to make this
+        // insensitive to the order in which the local-client and local-server are started
+        // we can't check if a local-server is running before doing this. This is fine
+        // because this won't do anything if there is no server running to accept clients.
         if (!server_side)
             steamworks_server_backend.register_local_client(this);
     }
 
     public steamworks_client_backend() : this(Steamworks.SteamClient.SteamId) { }
-
-    public override IAsyncResult BeginConnect(string address, int port)
-    {
-        return null;
-    }
-
-    public override void Close(int timeout_ms = 0)
-    {
-
-    }
-
+    public override IAsyncResult BeginConnect(string address, int port) { return null; }
+    public override void Close(int timeout_ms = 0) { }
     public override int ReceiveBufferSize => 16384;
     public override int SendBufferSize => 16384;
     public override string remote_address => "Steam user " + id.Value;
@@ -168,6 +195,10 @@ public class steamworks_client_backend : client_backend
         {
             if (_steamworks_stream == null)
             {
+                // Data from the server to a client is sent on channel 0
+                // Data from a client to the server is sent on channel 1
+                // This is so messages from a local-server to a local-client
+                // (i.e messages from the server to itself) aren't mixed up
                 int send_channel = server_side ? 0 : 1;
                 int receive_channel = server_side ? 1 : 0;
 
--- Assets/code/server_backend.cs
+++ Assets/code/server_backend.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
+/// <summary> The interface needed for the server to communicate with clients. </summary>
 public abstract class server_backend
 {
+    /// <summary> Start listening for incomming messages (start the server). </summary>
     public abstract void Start();
+
+    /// <summary> Stop listening to messages (stop the server). </summary>
     public abstract void Stop();
+
+    /// <summary> Are there pending incomming connections? </summary>
+    /// <returns>True if there are pending incoming connections.</returns>
     public abstract bool Pending();
+
+    /// <summary> Call to accept a pending incomming connection. </summary>
+    /// <returns>The client that was trying to connect.</returns>
     public abstract client_backend AcceptClient();
+
+    /// <summary> A string describing the address that this backend is listening on. </summary>
     public abstract string local_address { get; }
 
+    //##############//
+    // STATIC STUFF //
+    //##############//
+
+    /// <summary> The default backend used. </summary>
     public static server_backend default_backend()
     {
         return new tcp_server_backend(network_utils.local_ip_address(), server.DEFAULT_PORT);
@@ -18,15 +35,11 @@ public abstract class server_backend
     }
 }
 
+/// <summary> A TCP implementation of the server backend. </summary>
 public class tcp_server_backend : server_backend
 {
     TcpListener listener;
-
-    public tcp_server_backend(IPAddress address, int port)
-    {
-        listener = new TcpListener(address, port);
-    }
-
+    public tcp_server_backend(IPAddress address, int port) { listener = new TcpListener(address, port); }
     public override void Start() => listener.Start();
     public override void Stop() => listener.Stop();
     public override bool Pending() => listener.Pending();
@@ -34,8 +47,10 @@ public class tcp_server_backend : server_backend
     public override client_backend AcceptClient() => new tcp_client_backend(listener.AcceptTcpClient());
 }
 
+/// <summary> A Steam P2P networking implementation of the server backend. </summary>
 public class steamworks_server_backend : server_backend
 {
+    /// <summary> Remote steam users awaiting connection. </summary>
     Queue<Steamworks.SteamId> pending_users = new Queue<Steamworks.SteamId>();
 
     public override void Start()
@@ -43,6 +58,7 @@ public class steamworks_server_backend : server_backend
         if (!Steamworks.SteamClient.IsLoggedOn)
             throw new System.Exception("Steam not connected!");
 
+        // Accept new P2P connections
         Steamworks.SteamNetworking.OnP2PSessionRequest = (new_id) =>
         {
             Steamworks.SteamNetworking.AcceptP2PSessionWithUser(new_id);
@@ -50,31 +66,39 @@ public class steamworks_server_backend : server_backend
         };
     }
 
-    public override void Stop()
-    {
+    public override void Stop() { } // Nothing to do
 
-    }
-
+    /// <summary> Either remote steam users are awaiting connection, 
+    /// or the local client is awaiting connection. </summary>
     public override bool Pending() => pending_users.Count > 0 || local_client_awating_accept;
 
     public override client_backend AcceptClient()
     {
         if (local_client_awating_accept)
         {
+            // Accept local client
             local_client_awating_accept = false;
             return local_client;
         }
 
+        // Accept remote client
         var id = pending_users.Dequeue();
         return new steamworks_client_backend(id, server_side: true);
     }
 
+    /// <summary> For steam P2P communication, the address is basically just the steam user. </summary>
     public override string local_address => "Steam user " + Steamworks.SteamClient.SteamId.Value;
 
     //##############//
     // STATIC STUFF //
     //##############//
 
+    static steamworks_client_backend local_client = null;
+    static bool local_client_awating_accept = false;
+
+    /// <summary> Call to register a local steam user. This is neccassary because the local
+    /// steam client does not send a P2PSessionRequest to itself, but the server
+    /// still needs to accept the local client. </summary>
     public static void register_local_client(steamworks_client_backend client)
     {
         if (local_client != null)
@@ -83,7 +107,4 @@ public class steamworks_server_backend : server_backend
         local_client = new steamworks_client_backend(client.id, server_side: true);
         local_client_awating_accept = true;
     }
-
-    static steamworks_client_backend local_client = null;
-    static bool local_client_awating_accept = false;
 }
