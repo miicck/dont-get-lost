1093d35abd3ec4ca04dacc13009c81093d477fc9
miicck <michael.hutcheon@hotmail.co.uk>
2022-03-06 16:20:58 +0000

    Added small window for on-terrain test

--- Assets/code/attacker_entrypoint.cs
+++ Assets/code/attacker_entrypoint.cs
@@ -155,8 +155,13 @@ public class attacker_entrypoint : MonoBehaviour, INonEquipable, INonBlueprintab
     //#########//
 
     // Is the vector v considered to be on the terrain
-    bool point_on_terrain(Vector3 v) => utils.raycast_for_closest<Terrain>(
-        new Ray(v, Vector3.down), out RaycastHit hit, max_distance: 1f) != null;
+    bool point_on_terrain(Vector3 v)
+    {
+        var terr = utils.raycast_for_closest<Terrain>(
+            new Ray(v + 0.1f * Vector3.up, Vector3.down),
+            out RaycastHit hit, max_distance: 1.1f);
+        return terr != null;
+    }
 
     bool midpoint_success(Vector3 v) => (transform.position - v).magnitude > MAX_EXTERNAL_PATH_DISTANCE && point_on_terrain(v);
     bool endpoint_success(Vector3 v) => (transform.position - v).magnitude > MIN_EXTERNAL_PATH_DISTANCE && point_on_terrain(v);
