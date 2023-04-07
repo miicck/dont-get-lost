213ad5b54c0064845b17447824963c6e268d6d9f
miicck <michael.hutcheon@hotmail.co.uk>
2023-04-07 10:13:08 +0100

    Added discord button to main menu

---
+++ Assets/code/discord_button.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class discord_button : MonoBehaviour
+{
+    public void open_discord()
+    {
+        Application.OpenURL("https://discord.gg/xcfn9Q7KHh");
+    }
+}
